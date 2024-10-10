8c1802653330a4a518d2b2a43f59f91f63d48fa4
Alexis Bernthal <129408149+alexisbernt@users.noreply.github.com>
2024-10-10 17:05:07 -0500

    Create Board.js

---
+++ first-react/components/Board.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Square from "../Square";
+
+function Board() {
+
+    return <div className="board">
+        <Square/>
+    </div>
+    return <div className="board">
+        <Square/>
+    </div>
+}
+
+export default Board;
