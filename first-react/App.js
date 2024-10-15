cb6b357c6d64398a0dcadf5c1dca7b4cfbe21db6
Alexis Bernthal <129408149+alexisbernt@users.noreply.github.com>
2024-10-15 16:49:24 -0500

    Pokemon web dev

--- first-react/App.js
+++ first-react/App.js
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 import Board from './components/Board';
 import Header from './Header';
+import Config from './components/Config';
+import PokemonList from './components/PokemonList';
 
 function App() {
+  const [showBoxes, setShowBoxes] = useState(false);
   return (
     <div className="App">
        <Header />
-       <Board />
+       <Config showBoxes={showBoxes} setShowBoxes={setShowBoxes}/>
+       {showBoxes && <Board />}
+       <PokemonList/> 
     </div>
   );
 }
